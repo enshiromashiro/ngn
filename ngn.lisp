1a050c7cb07a615284daed7a538ac60799c6c623
subaru45 <devnull@localhost>
2013-05-20 00:56:56 +0900

    Refactored the codes parsing from tags.

--- ngn.lisp
+++ ngn.lisp
@@ -11,15 +11,24 @@
 
 (ql:quickload :cl-ppcre)
 
-(defconstant +regex-oneline-tag+ ":(.+?)[ ]+(.+)")
-(defconstant +regex-block-tag+ ":(.+?)(\\[|\\])")
+(defconstant +tag-block-delimiter-start+ #\[)
+(defconstant +tag-block-delimiter-end+ #\])
+
+(defconstant +tag-regex-oneline+ ":(.+?)[ ]+(.+)$")
+(defconstant +tag-regex-block+
+  (concatenate 'string
+			   ":(.+?)(\\"
+			   (string +tag-block-delimiter-start+)
+			   "|\\"
+			   (string +tag-block-delimiter-end+)
+			   ")$"))
 
 
 (defun guess-encoding (filepath)
-  nil)
+  :utf-8)
 
 (defun guess-line-termination (filepath)
-  nil)
+  :windows)
 
 (defun gen-keyword (name)
   (car (multiple-value-list (intern (string-upcase name)
@@ -30,54 +39,44 @@
 	  str
 	  nil))
 
-(defun block-p (tag)
-  (if (null tag)
+(defun scan-tag (line tag-regex)
+  (if (and (stringp line) (empty-string-p line))
 	  nil
-	  (let ((data (cadr tag)))
-		(or (string= data "[")
-			(string= data "]")))))
+	  (multiple-value-bind (_ tag) (cl-ppcre:scan-to-strings tag-regex line)
+		(if (null _)
+			nil
+			(list (gen-keyword (svref tag 0)) (svref tag 1))))))
 
-(defun parse-oneline-tag (line)
-  (multiple-value-bind (_ tags)
-	  (cl-ppcre:scan-to-strings +regex-oneline-tag+ line)
-	(coerce tags 'list)))
-
-(defun parse-block-tag (line)
-  (multiple-value-bind (_ tags)
-	  (cl-ppcre:scan-to-strings +regex-block-tag+ line)
-	(coerce tags 'list)))
-
-(defun scan-tag (line)
-  (if (empty-string-p line)
-	  nil
-	  (let ((btag (parse-block-tag line)))
-		(if (null btag)
-			(parse-oneline-tag line)
-			btag))))
-
-
-;; Experimental code.
-;; TODO: test. refactoring.
-(defun get-tag-data (text)
-  (let ((data))
-	(do ((i 0 (1+ i)))
-		((>= i (length text)) (reverse data))
-	  (let ((tag (scan-tag (nth i text))))
+(defun scan-oneline-tags (text)
+  (let ((tags))
+	(dolist (line text (reverse tags))
+	  (let ((tag (scan-tag line +tag-regex-oneline+)))
 		(if (not (null tag))
-			(if (block-p tag)
-				(push (list (gen-keyword (car tag))
-							(let ((d))
-							  (incf i)
-							  (do ((line (nth i text) (nth i text)))
-								  ((block-p (scan-tag line)))
-								(push line d)
-								(incf i))
-							  (reverse d)))
-					  data)
-				(push (list (gen-keyword (car tag))
-							(cadr tag))
-					  data)))))))
+			(push tag tags))))))
 
+(defun scan-block-tags (text &optional tag-name lines)
+  (if (null text)
+	  nil
+	  (flet ((tag-p (tag delim)
+			   (and (not (null tag)) (string= (cadr tag) delim))))
+		(if (null tag-name)
+			(let ((tag (scan-tag (car text) +tag-regex-block+)))
+			  (if (tag-p tag +tag-block-delimiter-start+)
+				  (scan-block-tags (cdr text) (car tag))
+				  (scan-block-tags (cdr text))))
+			(let ((tag (scan-tag (car text) +tag-regex-block+)))
+			  (if (and (tag-p tag +tag-block-delimiter-end+)
+					   (eq tag-name (gen-keyword (car tag))))
+				  (cons (list tag-name lines)
+						(scan-block-tags (cdr text)))
+				  (scan-block-tags (cdr text) 
+								   tag-name
+								   (reverse (cons (car text)
+												  (reverse lines))))))))))
+
+(defun scan-tags (text)
+  (append (scan-oneline-tags text)
+		  (scan-block-tags text)))
 
 (defun read-text (filepath)
   (let* ((text)
