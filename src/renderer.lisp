26f99865e91a71ce2f2039f4afa39487e7cfb544
subaru45 <devnull@localhost>
2015-02-22 14:22:24 +0900

    Three changes. First, render interface was changed. Template file is taken as stream. Second, Renderer filepath determination is moved to upstage. Third, fixes a bug, that is no output line when the line of template has no 'markers'

--- src/renderer.lisp
+++ src/renderer.lisp
@@ -19,17 +19,6 @@
 (cl-annot:enable-annot-syntax)
 
 
-(defvar *renderer-dirname* "render/")
-
-(defun make-renderer-path
-    (tmppath &optional (runtime-path *runtime-pathname*))
-  (merge-pathnames
-   (concatenate 'string
-                (string-downcase (pathname-type tmppath))
-                "-renderer.lisp")
-   (merge-pathnames *renderer-dirname* runtime-path)))
-
-
 (defun make-marker-regex (name)
   (etypecase name
     (string (format nil "#\\|~a\\|#" name))
@@ -46,25 +35,24 @@
 
 (defun embed-into-template (tags in out)
   (flet ((readline () (read-line in nil :eof)))
-    (loop for line = (readline) then (readline)
+    (loop
+       for line = (readline)
        for outline = line then line
        until (eq line :eof)
-       do (dolist (tag (included-markers tags line))
-            (setf outline
-                  (regex-replace-all (make-marker-regex tag)
-                                     outline
-                                     (gethash tag tags)))
-            (write-line outline out)))))
+       do (let ((markers (included-markers tags line)))
+            (if (null markers)
+                (write-line line out)
+                (dolist (tag (included-markers tags line))
+                  (setf outline
+                        (regex-replace-all (make-marker-regex tag)
+                                           outline
+                                           (gethash tag tags)))
+                  (write-line outline out)))))))
 
 
 @export
-(defun render (tags tmppath out-stream)
-  "render text from _tags_ and _tmppath_ to _out-stream_ with `renderer`.
-_tag_ is a hashtable. _tmppath_ is a pathname.
-`renderer` is a file include tag"
-  (let ((ef (detect-external-format tmppath :jp)))
-    (with-open-file (in-stream tmppath
-                     :direction :input
-                     :external-format ef)
-      (embed-into-template (eval-tags tags (make-renderer-path tmppath))
-                           in-stream out-stream))))
+(defun render (tags outstream tmpstream rndrpath)
+  "render text from _tags_ and _tmpstream_ to _outstream_ with `renderer` at _rndpath_.
+_tag_ is a hashtable. 
+`renderer` is a file include functions called by render-dsl module"
+  (embed-into-template (eval-tags tags rndrpath) tmpstream outstream))
