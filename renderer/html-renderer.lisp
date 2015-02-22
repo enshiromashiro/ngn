78de5e073e51c38074a795e00c4d4bef66242d01
subaru45 <devnull@localhost>
2015-02-23 02:32:45 +0900

    Adds an HTML renderer

---
+++ renderer/html-renderer.lisp
@@ -0,0 +1,45 @@
+;;;; ngn renderer for html
+;;;; In this file, you can use functions and macros in CL-USER package
+
+
+(require :cl-ppcre)
+
+;; per-line rendering
+(defun render-per-line (line)
+  (if (or (ppcre:scan "^#+ " line)
+          (ppcre:scan "\"$" line))
+      line
+      (format nil "~a<br>" line)))
+
+;; ruby rendering
+(defun render-ruby (text ruby)
+  (format nil "<ruby>~a<rp>（</rp><rt>~a</rt><rp>）</rp></ruby>" text ruby))
+
+;; emphasis rendering
+(defun render-emphasis (text)
+  (format nil "<span class=\"emphasis\">~a</span>" text))
+
+;; bold rendering
+(defun render-bold (text)
+  (format nil "<span class=\"bold\">~a</span>" text))
+
+;; italic rendering
+(defun render-italic (text)
+  (format nil "<span class=\"italic\">~a</span>" text))
+
+;; bold-italic rendering
+(defun render-bold-italic (text)
+  (format nil "<span class=\"bold-italic\">~a</span>" text))
+
+;; underline rendering
+(defun render-underline (text)
+  (format nil "<span class=\"underline\">~a</span>" text))
+
+;; header rendering
+(defun render-header (text level)
+  (format nil "<h2 class=\"header~a\">~a</h2>" level text))
+
+;; quote rendering
+(defun render-quote (text level)
+  (format nil "<p class=\"quote~a\">~%~a~%</p>" level text))
+
