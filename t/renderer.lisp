315cfb560726d2767521dcfa26c702540ec95b65
subaru45 <devnull@localhost>
2015-03-10 22:43:27 +0900

    Adds tests about renderer

--- t/renderer.lisp
+++ t/renderer.lisp
@@ -11,9 +11,63 @@
 (in-package :ngn.renderer-test)
 
 
-(plan nil)
+(plan 3)
 
 
-(run-test-all)
+(subtest "Testing make-marker-regex"
+  (flet ((call-it (name)
+           (ngn.renderer::make-marker-regex name)))
+
+    (is (call-it "test") "#\\|test\\|#")
+    (is (call-it "TEST") "#\\|test\\|#")
+    (is (call-it 'test) "#\\|test\\|#")
+    (is (call-it '|test|) "#\\|test\\|#")))
+
+
+(subtest "Testing included-markers"
+  (flet ((call-it (tags line)
+           (ngn.renderer::included-markers tags line)))
+    (let ((hash (make-hash-table)))
+      (setf (gethash 'body hash) "本文")
+
+      (is (call-it hash "#|body|#") '(body))
+      (is (call-it hash "#|TITLE|#") '())
+      (is (call-it hash "#|title|# #|body|#") '(body)))
+
+    (let ((hash (make-hash-table)))
+      (setf (gethash 'body hash) "本文")
+      (setf (gethash 'title hash) "題名")
+
+      (is (call-it hash "#|body|#") '(body))
+      (is (call-it hash "#|TITLE|#") '())
+      (is (call-it hash "#|title|# #|body|#") '(body title)))))
+
+
+(subtest "Testing #'embed-into-template"
+  (flet ((call-it (str tags)
+           (with-input-from-string (in str)
+             (with-output-to-string (out)
+               (ngn.renderer::embed-into-template tags in out)))))
+
+    (let ((hash (make-hash-table)))
+      (setf (gethash 'title hash) "題名")
+      (setf (gethash 'body hash) "本文")
+
+      (is (call-it "<html><head>
+<title>#|title|#</title>
+</head>
+<body>
+<h1>#|titl|# - これはタイポ</h1>
+#|body|#
+</body></html>" hash)
+          "<html><head>
+<title>題名</title>
+</head>
+<body>
+<h1>#|titl|# - これはタイポ</h1>
+本文
+</body></html>
+"))))
+
 
 (finalize)
