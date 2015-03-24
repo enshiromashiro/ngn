a79c744f0b555938bf5118f5eacc55a6015213b4
subaru45 <devnull@localhost>
2015-03-24 23:25:26 +0900

    Adds tests for auto-indexing

--- t/renderers/html-renderer.lisp
+++ t/renderers/html-renderer.lisp
@@ -10,11 +10,89 @@
 (in-package :ngn.renderer.html-renderer-test)
 
 
-(plan 9)
+(plan 12)
 
 ;;; loading html-renderer
 (load "renderer/html-renderer.lisp")
 
+
+;;; for auto indexing
+(subtest "%add-leaf"
+  (subtest "adding a leaf; to empty-tree (=nil)"
+    (is (%add-leaf 'x 1 nil) '(nil x))
+    (is (%add-leaf 'x 2 nil) '(nil (nil x)))
+    (is (%add-leaf 'x 4 nil) '(nil (nil (nil (nil x))))))
+
+  (subtest "adding a leaf; is always added to _next position of last leaf_"
+    (is (%add-leaf 'x 1 '(nil a))
+        '(nil a x))
+    (is (%add-leaf 'x 1 '(nil a b c))
+        '(nil a b c x))
+    (is (%add-leaf 'x 1 '(nil (a b) c d))
+        '(nil (a b) c d x)))
+
+  (subtest "adding a leaf; to any depth"
+    (is (%add-leaf 'x 2 '(nil a))
+        '(nil (a x)))
+    (is (%add-leaf 'x 3 '(nil a))
+        '(nil (a (nil x))))
+    (is (%add-leaf 'x 2 '(nil (a b) (c d)))
+        '(nil (a b) (c d x)))
+    (is (%add-leaf 'x 3 '(nil (a (b c)) (d (e f g))))
+        '(nil (a (b c)) (d (e f g x))))
+    (is (%add-leaf 'x 4 '(nil (a (b c)) (d (e f g))))
+        '(nil (a (b c)) (d (e f (g x)))))))
+
+(subtest "add-header"
+  (setf *-index-* nil
+        *header-number* 1)
+  (flet ((idx (id txt)
+           (make-index-elem :id id :text txt)))
+    (is *-index-* nil)
+    (is *header-number* 1)
+
+    (add-header 1 "header1")
+    (is *-index-* `(nil ,(idx 1 "header1"))
+      :test #'equalp)
+    (is *header-number* 2)
+
+    (add-header 2 "header2")
+    (is *-index-* `(nil (,(idx 1 "header1")
+                         ,(idx 2 "header2")))
+        :test #'equalp)
+    (is *header-number* 3)))
+
+(subtest "rendering index"
+  (flet ((idx (id txt)
+           (make-index-elem :id id :text txt)))
+    (is (render-index nil) "")
+    (is (render-index '(nil)) "")
+
+    (is (render-index `(nil ,(idx 1 "chapter1")
+                            (,(idx 2 "chapter2")
+                             ,(idx 3 "chapter2.1")
+                             ,(idx 4 "chapter2.2"))
+                            (nil
+                             ,(idx 5 "chapter3.1"))))
+        "<ol>
+<li><a href=\"#sec1\">chapter1</a></li>
+<li><a href=\"#sec2\">chapter2</a>
+  <ol>
+  <li><a href=\"#sec3\">chapter2.1</a></li>
+  <li><a href=\"#sec4\">chapter2.2</a></li>
+  </ol>
+</li>
+<li>
+  <ol>
+  <li><a href=\"#sec5\">chapter3.1</a></li>
+  </ol>
+</li>
+</ol>
+")))
+
+
+
+;;; normal renderer api
 (subtest "per-line rendering"
   (is (render-per-line "") "<br>")
   (is (render-per-line "line") "line<br>")
@@ -59,8 +137,18 @@
       "<span class=\"underline\">text</span>"))
 
 (subtest "rendering header"
-  (is (render-header "" 1) "<h1></h1>")
-  (is (render-header "text" 1) "<h1>text</h1>"))
+  (setf *-index-* nil
+        *header-number* 1)
+  (is (render-header "" 1) "<h1><a name=\"sec1\"></a></h1>")
+  (is *-index-* `(nil ,(make-index-elem :id 1 :text ""))
+      :test #'equalp)
+  (is *header-number* 2)
+
+  (is (render-header "text" 1) "<h1><a name=\"sec2\">text</a></h1>")
+  (is *-index-* `(nil ,(make-index-elem :id 1 :text "")
+                      ,(make-index-elem :id 2 :text "text"))
+      :test #'equalp)
+  (is *header-number* 3))
 
 (subtest "rendering quotation"
   (is (render-quote "" 1)
