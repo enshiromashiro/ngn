4079812e894b78fea9e326d3b7a86cc7de83f67b
subaru45 <devnull@localhost>
2015-03-24 23:36:52 +0900

    Can generate index automatically!

--- renderer/html-renderer.lisp
+++ renderer/html-renderer.lisp
@@ -4,6 +4,77 @@
 
 (require :cl-ppcre)
 
+
+;;; for auto-indexing
+(defparameter *-index-* nil)
+(defparameter *header-number* 1)
+
+(defstruct index-elem
+  id text)
+
+(defun %add-leaf (elem level tree &optional (lev 1))
+  (if (eq level lev)
+      (if (atom tree)
+          (list tree elem)
+          (append tree (list elem)))
+      (if (atom tree)
+          (list tree
+                (%add-leaf elem level nil (1+ lev)))
+          (append (list (car tree))
+                  (butlast (cdr tree))
+                  (list (%add-leaf elem level
+                                   (car (last (cdr tree))) (1+ lev)))))))
+
+(defun add-header (level text)
+  (setf *-index-*
+        (%add-leaf (make-index-elem :id *header-number*
+                                    :text text)
+                   level
+                   *-index-*))
+  (incf *header-number*))
+
+(defun render-elem (index-elem &optional (end? t))
+  (with-output-to-string (out)
+    (format out "<li>")
+    (format out "<a href=\"#sec~a\">~a</a>"
+            (index-elem-id index-elem)
+            (index-elem-text index-elem))
+    (when end?
+      (format out "</li>"))
+    (format out "~%")))
+
+(defun %render-index (index &optional (level 0))
+  (with-output-to-string (out)
+    (unless (null index)
+      (let ((tab (format nil "~~~a,0T" (* 2 level))))
+        (flet ((%format (fmt &rest rest)
+                 (apply #'format
+                        `(,out ,(concatenate 'string tab fmt) ,@rest))))
+          (%format "<ol>~%")
+          (loop
+             for e in index
+             unless (listp e) do
+               (%format (render-elem e))
+             else do
+               (%format "~a~a</li>~%"
+                        (if (null (car e))
+                            (format nil "<li>~%")
+                            (render-elem (car e) nil))
+                        (%render-index (cdr e) (1+ level))))
+          (%format "</ol>~%"))))))
+
+(defun render-index (index)
+  (%render-index (cdr index)))
+
+(defun add-index (hash)
+  (setf (gethash '-index- hash)
+        (render-index *-index-*)))
+
+(setf *render-hook* #'add-index)
+
+
+
+;;; render functions
 ;; per-line rendering
 (defun render-per-line (line)
   (if (ppcre:scan "^#\{1,4\} " line)
@@ -36,7 +107,10 @@
 
 ;; header rendering
 (defun render-header (text level)
-  (format nil "<h2 class=\"header~a\">~a</h2>" level text))
+  (prog1
+      (format nil "<h~a><a name=\"sec~a\">~a</a></h~a>"
+              level *header-number* text level)
+    (add-header level text)))
 
 ;; quote rendering
 (defun render-quote (text level)
